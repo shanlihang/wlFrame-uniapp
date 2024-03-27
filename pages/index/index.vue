505e95b2e452372a55d0be2847c7a9531ce69ed2
shanlihang <slh_ontuo@126.com>
2024-03-27 16:40:53 +0800

    引入wot design uni

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,6 +3,11 @@
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
+			<wd-button>主要按钮</wd-button>
+			<wd-button type="success">成功按钮</wd-button>
+			<wd-button type="info">信息按钮</wd-button>
+			<wd-button type="warning">警告按钮</wd-button>
+			<wd-button type="error">危险按钮</wd-button>
 		</view>
 	</view>
 </template>
