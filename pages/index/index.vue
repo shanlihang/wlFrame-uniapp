50ccd4a49bb9a70458c2a156eddc60885572abfb
shanlihang <slh_ontuo@126.com>
2024-04-15 15:28:50 +0800

    测试换肤

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,6 @@
 <template>
-<<<<<<< HEAD
 	<view class="a">
 		<FloatNav></FloatNav>
-=======
-	<view class="index">
-		<FloatNav>
-			<FloatNavItem url="/pages/index/index" name="首页" color="#E0E0E0" />
-			<FloatNavItem url="/pages/other/other" name="其他" />
-			<FloatNavItem url="/pages/home/home" name="我的" />
-		</FloatNav>
->>>>>>> b3991cc89cfef9b1626fbfc5b62881acad2ad1c3
 	</view>
 </template>
 
