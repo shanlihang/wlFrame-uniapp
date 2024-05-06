2df17c0945d7610de6f0fcef233f840507306132
shanlihang <slh_ontuo@126.com>
2024-05-06 13:14:26 +0800

    悬浮按钮得设置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,9 @@
 <template>
 	<view class="index">
-		首页
+		<wd-fab type="success" position="right-bottom" direction="top" :draggable="true">
+			<view class="item">活动</view>
+			<view class="item" style="margin-bottom:10rpx;">打卡</view>
+		</wd-fab>
 	</view>
 </template>
 
@@ -13,5 +16,17 @@
 .index {
 	width: 100%;
 	height: 100vh;
+	
+}
+.item{
+	width: 70rpx;
+	height: 70rpx;
+	border-radius:50%;
+	background-color: #31AE37;
+	display:flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 24rpx;
+	color:#E0E0E0;
 }
 </style>
