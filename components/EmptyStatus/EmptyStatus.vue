d6a1b47a5226ecebfaa87d94792f4c5516256fae
shanlihang <slh_ontuo@126.com>
2024-05-21 17:45:43 +0800

    预搭建空状态组件

---
+++ components/EmptyStatus/EmptyStatus.vue
@@ -0,0 +1,16 @@
+<template>
+	<view class="empty">
+		123
+	</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped lang="less">
+.empty{
+	width: 100%;
+	height: 100%;
+}
+</style>
