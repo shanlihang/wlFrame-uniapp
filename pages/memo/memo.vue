6b3ede7bbcd8e78161e6f511c44e9f7304f3ddb8
slh_home <slh67490009@gmail.com>
2024-06-22 22:25:10 +0800

    完成创建备忘录页面格式的设计

--- pages/memo/memo.vue
+++ pages/memo/memo.vue
@@ -2,15 +2,16 @@
 	<view class="memo">
 		<wd-form ref="form" :model="addForm">
 			<wd-cell-group border>
-				<wd-cell title="备忘录标题" center required>
+				<wd-cell title-width="170rpx" title="备忘录标题" center required>
 					<wd-input
+						custom-input-class="input"
 						no-border
 						clearable
 						v-model="addForm.title"
 						placeholder="请输入备忘录标题"
 					/>
 				</wd-cell>
-				<wd-cell title="备忘录内容" required vertical>
+				<wd-cell title-width="170rpx" title="备忘录内容" required vertical>
 					<wd-textarea
 					    clearable
 						no-border
@@ -23,10 +24,16 @@
 				
 			</wd-cell-group>
 			<wd-gap bg-color="#F3F2F7" height="50rpx"></wd-gap>
-			<wd-cell-group>
-				<wd-cell title="提醒时间" label="不设置提醒时间则不进行消息提醒" center>
-					<wd-datetime-picker v-model="addForm.time" :default-value="Date.now()" />
+			<wd-cell-group border>
+				<wd-cell title-width="170rpx" title="开启提醒" center>
+					<wd-switch v-model="flag" size="24px" active-color="#31AE37" @change="changeFlag" />
 				</wd-cell>
+				<template v-if="flag">
+					<wd-cell title-width="170rpx" title="提醒时间" center>
+						<wd-datetime-picker v-model="addForm.time" :default-value="Date.now()" placeholder="请选择提醒时间" />
+					</wd-cell>
+				</template>
+				
 			</wd-cell-group>
 			<wd-gap bg-color="#F3F2F7" height="50rpx"></wd-gap>
 		</wd-form>
@@ -47,6 +54,8 @@ interface AddForm{
 
 const form = ref()
 
+const flag = ref<boolean>(false)
+
 const addForm = reactive<AddForm>({
 	title:'',
 	content:'',
@@ -55,6 +64,14 @@ const addForm = reactive<AddForm>({
 
 const handleSubmit = () => {
 	console.log(addForm);
+	uni.switchTab({
+		url:'/pages/index/index'
+	})
+}
+
+const changeFlag = ({value}) => {
+	if(value) return;
+	addForm.time = ''
 }
 </script>
 
@@ -62,8 +79,12 @@ const handleSubmit = () => {
 .memo{
 	width: 100vw;
 	height: 100vh;
+	.input{
+		text-align: end;
+	}
 	.btn{
 		margin: 0 24rpx;
 	}
 }
+
 </style>
