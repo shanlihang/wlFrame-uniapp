fef022e9d4c2b8c2108760b10f8e9ae4c36c6012
slh_home <slh67490009@gmail.com>
2024-07-02 20:16:15 +0800

    增加其余页面的进入入口

--- pages/plan/plan.vue
+++ pages/plan/plan.vue
@@ -1,12 +1,18 @@
 <template>
 	<view class="plan">
-		计划
+		<button @click="open('/pages/login/login')">登录</button>
+		<button @click="open('/pages/avatarAndName/avatarAndName')">选择头像昵称</button>
 	</view>
 </template>
 
 
 <script setup lang="ts">
 
+const open = (url:string) => {
+	uni.navigateTo({
+		url:url
+	})
+}
 </script>
 
 <style lang="less" scoped>
