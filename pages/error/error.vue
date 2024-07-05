b46fb2dadee35a60cf83517732542df420753a26
slh_home <slh67490009@gmail.com>
2024-07-05 10:35:07 +0800

    新增错误界面

---
+++ pages/error/error.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="pageNotFound">
+		<PageNotFound />
+	</view>
+</template>
+
+<script setup lang="ts">
+import PageNotFound from './components/PageNotFound.vue'
+</script>
+
+<style scoped lang="less">
+.pageNotFound{}
+</style>
--- pages/plan/plan.vue
+++ pages/plan/plan.vue
@@ -2,6 +2,8 @@
 	<view class="plan">
 		<button @click="open('/pages/login/login')">登录</button>
 		<button @click="open('/pages/avatarAndName/avatarAndName')">选择头像昵称</button>
+		<button @click="open('/pages/a/a')">跳转不存在的页面</button>
+		<button @click="open('/pages/error/error')">不存在的页面</button>
 	</view>
 </template>
 
